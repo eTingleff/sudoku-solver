97cc260306d488d0da38b73fb42246bec2130d5b
Eric Tingleff <etingleff@gmail.com>
2022-06-11 01:03:40 -0700

    add unit/functional tests, add test strings

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,8 +2,133 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const {
+  puzzlesAndSolutions,
+  invalidPuzzles,
+} = require('../controllers/puzzle-strings');
+
+let solver = new Solver();
 
 suite('UnitTests', () => {
 
+  test('Logic handles a valid puzzle string of 81 characters', (done) => {
+    const puzzleString = puzzlesAndSolutions[0][0];
+    const result = solver.validate(puzzleString);
+    assert.deepEqual(result, { valid: true });
+
+    done();
+  });
+
+  test('Logic handles a puzzle string with invalid characters (not 1-9 or .)', (done) => {
+    const puzzleString = invalidPuzzles.invalidCharacters;
+    const result = solver.validate(puzzleString);
+    assert.deepEqual(result, { error: 'Invalid characters in puzzle' });
+
+    done();
+  });
+
+  test('Logic handles a puzzle string that is not 81 characters in length', (done) => {
+    const puzzleString = invalidPuzzles.invalidLength;
+    const result = solver.validate(puzzleString);
+    assert.deepEqual(result, { error: 'Expected puzzle to be 81 characters long' });
+
+    done();
+  });
+
+  test('Logic handles a valid row placement', (done) => {
+    const puzzleString = puzzlesAndSolutions[0][0];
+    const row = 'E';
+    const col = '4';
+    const value = '9';
+    const result = solver.checkRowPlacement(puzzleString, row, col, value);
+    assert.isTrue(result);
+
+    done();
+  });
+
+  test('Logic handles an invalid row placement', (done) => {
+    const puzzleString = puzzlesAndSolutions[0][0];
+    const row = 'E';
+    const col = '4';
+    const value = '8';
+    const result = solver.checkRowPlacement(puzzleString, row, col, value);
+    assert.isFalse(result);
+
+    done();
+  });
+
+  test('Logic handles a valid column placement', (done) => {
+    const puzzleString = puzzlesAndSolutions[0][0];
+    const row = 'E';
+    const col = '4';
+    const value = '9';
+    const result = solver.checkColPlacement(puzzleString, row, col, value);
+    assert.isTrue(result);
+
+    done();
+  });
+
+  test('Logic handles an invalid column placement', (done) => {
+    const puzzleString = puzzlesAndSolutions[0][0];
+    const row = 'F';
+    const col = '1';
+    const value = '4';
+    const result = solver.checkColPlacement(puzzleString, row, col, value);
+    assert.isFalse(result);
+
+    done();
+  });
+
+  test('Logic handles a valid region (3x3 grid) placement', (done) => {
+    const puzzleString = puzzlesAndSolutions[0][0];
+    const row = 'F';
+    const col = '1';
+    const value = '1';
+    const result = solver.checkRegionPlacement(puzzleString, row, col, value);
+    assert.isTrue(result);
+
+    done();
+  });
+
+  test('Logic handles an invalid region (3x3 grid) placement', (done) => {
+    const puzzleString = puzzlesAndSolutions[0][0];
+    const row = 'F';
+    const col = '1';
+    const value = '9';
+    const result = solver.checkRegionPlacement(puzzleString, row, col, value);
+    assert.isFalse(result);
+
+    done();
+  });
+
+  test('Valid puzzle strings pass the solver', (done) => {
+    puzzlesAndSolutions.forEach((puzzle, index) => {
+      const result = solver.solve(puzzle[1]);
+      assert.equal(result, puzzle[1], `puzzle #${index + 1} failed`);
+    });
+
+    done();
+  });
+
+  test('Invalid puzzle strings fail the solver', (done) => {
+    const invalidCharactersResult = solver.solve(invalidPuzzles.invalidCharacters);
+    const invalidLengthResult = solver.solve(invalidPuzzles.invalidLength);
+    assert.deepEqual(invalidCharactersResult, {
+      error: 'Invalid characters in puzzle',
+    });
+    assert.deepEqual(invalidLengthResult, {
+      error: 'Expected puzzle to be 81 characters long',
+    });
+
+    done();
+  });
+
+  test('Solver returns the expected solution for an incomplete puzzle', (done) => {
+    puzzlesAndSolutions.forEach((puzzle, index) => {
+      const result = solver.solve(puzzle[0]);
+      assert.equal(result, puzzle[1], `puzzle #${index + 1} failed`);
+    });
+
+    done();
+  });
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -3,9 +3,233 @@ const chaiHttp = require('chai-http');
 const assert = chai.assert;
 const server = require('../server');
 
+const {
+  puzzlesAndSolutions,
+  invalidPuzzles,
+} = require('../controllers/puzzle-strings');
+
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
+
+  suite('POST request to /api/solve', () => {
+    const endpoint = '/api/solve';
+
+    test('Solve a puzzle with valid puzzle string', (done) => {
+      chai.request(server)
+        .post(endpoint)
+        .send({
+          puzzle: puzzlesAndSolutions[0][0],
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { puzzle: puzzlesAndSolutions[0][1] });
+
+          done();
+        });
+    });
+
+    test('Solve a puzzle with missing puzzle string', (done) => {
+      chai.request(server)
+        .post(endpoint)
+        .send({})
+        .end((err, res) => {
+          assert.deepEqual(res.body, { error: 'Required field missing' });
+
+          done();
+        });
+    });
+
+    test('Solve a puzzle with invalid characters', (done) => {
+      chai.request(server)
+        .post(endpoint)
+        .send({ puzzle: invalidPuzzles.invalidCharacters })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { error: 'Invalid characters in puzzle' });
+
+          done();
+        });
+    });
+
+    test('Solve a puzzle with incorrect length', (done) => {
+      chai.request(server)
+        .post(endpoint)
+        .send({ puzzle: invalidPuzzles.invalidLength })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { error: 'Expected puzzle to be 81 characters long' });
+
+          done();
+        });
+    });
+
+    test('Solve a puzzle that cannot be solved', (done) => {
+      chai.request(server)
+        .post(endpoint)
+        .send({ puzzle: invalidPuzzles.cannotBeSolved })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { error: 'Puzzle cannot be solved' });
+
+          done();
+        });
+    });
+
+  });
+
+  suite('POST request to /api/check', () => {
+    const endpoint = '/api/check';
+
+    test('Check a puzzle placement with all fields', (done) => {
+      chai.request(server)
+        .post(endpoint)
+        .send({
+          coordinate: 'A2',
+          puzzle: puzzlesAndSolutions[0][0],
+          value: '3',
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { valid: true });
+
+          done();
+        });
+    });
+
+    test('Check a puzzle placement with single placement conflict', (done) => {
+      chai.request(server)
+        .post(endpoint)
+        .send({
+          coordinate: 'A2',
+          puzzle: puzzlesAndSolutions[0][0],
+          value: '4',
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, {
+            valid: false,
+            conflicts: ['row'],
+          });
+
+          done();
+        });
+    });
+
+    test('Check a puzzle placement with multiple placement conflicts', (done) => {
+      chai.request(server)
+        .post(endpoint)
+        .send({
+          coordinate: 'A2',
+          puzzle: puzzlesAndSolutions[0][0],
+          value: '5',
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, {
+            valid: false,
+            conflicts: [
+              'row',
+              'region',
+            ],
+          });
+
+          done();
+        });
+    });
+
+    test('Check a puzzle placement with all placement conflicts', (done) => {
+      chai.request(server)
+        .post(endpoint)
+        .send({
+          coordinate: 'A2',
+          puzzle: puzzlesAndSolutions[0][0],
+          value: '2',
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, {
+            valid: false,
+            conflicts: [
+              'row',
+              'column',
+              'region',
+            ],
+          });
+
+          done();
+        });
+    });
+
+    test('Check a puzzle placement with missing required fields', (done) => {
+      chai.request(server)
+        .post(endpoint)
+        .send({
+          coordinate: 'B6',
+          puzzle: puzzlesAndSolutions[0][0],
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { error: 'Required field(s) missing' });
+
+          done();
+        });
+    });
+
+    test('Check a puzzle placement with invalid characters', (done) => {
+      chai.request(server)
+        .post(endpoint)
+        .send({
+          coordinate: 'A2',
+          puzzle: invalidPuzzles.invalidCharacters,
+          value: '5',
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { error: 'Invalid characters in puzzle' });
+
+          done();
+        });
+    });
+
+    test('Check a puzzle placement with incorrect length', (done) => {
+      chai.request(server)
+        .post(endpoint)
+        .send({
+          coordinate: 'A2',
+          puzzle: invalidPuzzles.invalidLength,
+          value: '5',
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, {
+            error: 'Expected puzzle to be 81 characters long',
+          });
+
+          done();
+        });
+    });
+
+    test('Check a puzzle placement with invalid placement coordinate', (done) => {
+      chai.request(server)
+        .post(endpoint)
+        .send({
+          coordinate: 'Z6',
+          puzzle: puzzlesAndSolutions[0][0],
+          value: '5',
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { error: 'Invalid coordinate' });
+
+          done();
+        });
+    });
+
+    test('Check a puzzle placement with invalid placement value', (done) => {
+      chai.request(server)
+        .post(endpoint)
+        .send({
+          coordinate: 'A2',
+          puzzle: puzzlesAndSolutions[0][0],
+          value: 'J',
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { error: 'Invalid value' });
+
+          done();
+        });
+    });
+
+  });
 
 });
 
